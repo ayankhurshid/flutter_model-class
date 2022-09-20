1bbfdf8aa5ab8a71ef507b2b86ce5b19f96588bb
ayankhurshid <khurshidayan18@gmail.com>
2022-09-20 16:39:00 +0500

    cgrf

---
+++ flutter_application_1/lib/model/homepaageModel.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+
+class HomePageModel {
+  var counter = 0.obs;
+  var countervalue=0.obs;
+}
---
+++ flutter_application_1/lib/views/homepage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+   HomePage({Key? key}) : super(key: key);
+final box =GetS
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
